523495d1de8b57f1c5d17786029887cee3d26305
Chad Luong <apple.luong1905@gmail.com>
2018-07-29 08:29:25 +0700

    update code session 7 & 8

---
+++ session8/common1.js
@@ -0,0 +1,8 @@
+// alert('Hello javascrip 1');
+var a = 10;
+document.write('Hello javscript 2');
+console.log(a);
+document.getElementById('demo1').innerHTML = 'Hello javscript 3';
+console.log('Hello javscript 3');
+a = a + 5;
+console.log(a);
---
+++ session8/common2.js
@@ -0,0 +1,25 @@
+var a;
+var b = 5;
+var c, d, e;
+var userName; //camel
+var user_name;
+// bien nay xai cho ten nguoi dung
+var myUserName;
+var my_user_name;
+// comment
+/* comment o day
+	tiep tuc comment
+*/
+// + - * % /
+/*
+Khai báo 2 biến a và b
+Tính các phép cộng trừ, 
+nhân chia  từ a và b và in ra kết quả
+*/
+var a = 9;
+var b = 5;
+document.write('Ket qua cua a + b la:');
+document.write(a + b);
+document.write("<br>");
+document.write('Ket qua cua a - b la:');
+document.write(a - b);
