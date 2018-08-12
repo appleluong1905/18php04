9e2cb6fed7c87312e49277fcd704e3942a2929a8
Chad Luong <apple.luong1905@gmail.com>
2018-08-12 10:27:18 +0700

    update jquery

---
+++ session13/js/common1.js
@@ -0,0 +1,9 @@
+$('#changeContent .change1').click(function(){
+	$('.demo1').text('Hello World');
+});
+$('.change2').click(function(){
+	$('img').attr('src','img/pic_bulbon.gif');
+});
+$('.change3').click(function(){
+	$('img').attr('src','img/pic_bulboff.gif');
+});
